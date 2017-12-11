d2190144ff1d9434c5700008040a05619898dfd1
badatfun <j.c.delafields@gmail.com>
2017-12-10 20:37:16 -0500

    Some styling tweaks. Added subtitle to header. Changed Title.

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,9 @@ const Header = () => {
 	return (
 		<div id="header">
 			<h1>HODLCON</h1>
+			<h5 style={{ opacity: 0.7 }}>
+				A running list of hacks in the cryptocurrency space.
+			</h5>
 			<h4>
 				(H<sub>old</sub> O<sub>n</sub> <sub>for</sub> D<sub>ear</sub> L<sub>ife</sub>{' '}
 				<sub>readiness</sub> CON<sub>dition</sub>)
--- hacks.js
+++ hacks.js
@@ -35,8 +35,9 @@ export const hacks = [
 		amount: 2,
 		amountAbbreviated: '1m',
 		date: [2007, 3, 28],
-		description:
-			'<p>I typed dis text to a girl i used to see sayin that i saw dis cutie pie wit whom i wanna be</p>'
+		description: `
+			<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. </p>
+			`
 	},
 	{
 		name: 'Test',
