51cc8125fabd38c6d194a8ef6da598b013c46baf
badatfun <j.c.delafields@gmail.com>
2018-01-11 21:28:31 -0500

    Modified header

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,14 @@ const Header = ({ lastUpdate }) => {
 		<div id="header">
 			<h1>HODLCON</h1>
 			<h5 style={styles.subtitle}>
-				A running list of hacks in the cryptocurrency space.
+				A running list of noteworthy hacks and scams in the cryptocurrency
+				space.
 			</h5>
 			<h5 style={styles.subtitle}>Last updated {lastUpdate}</h5>
 			<h4>
-				(H<sub>old</sub> O<sub>n</sub> <sub>for</sub> D<sub>ear</sub> L<sub>ife</sub>{' '}
+				(H<sub>old</sub> O<sub>n</sub> <sub>for</sub> D<sub>ear</sub> L<sub>
+					ife
+				</sub>{' '}
 				<sub>readiness</sub> CON<sub>dition</sub>)
 			</h4>
 		</div>
