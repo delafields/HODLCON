d1c6bae6f50eebb7ed61e9d4796dcbf1a9e6cc8f
badatfun <j.c.delafields@gmail.com>
2018-02-12 23:31:29 -0500

    Setup testing env

---
+++ src/tests/Header.test.js
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact';
+import Header from '../components/Header';
+// See: https://github.com/mzgoddard/preact-render-spy
+import { shallow, deep } from 'preact-render-spy';
+
+describe('Initial Test of the Header', () => {
+	test('Header renders 3 nav items', () => {
+		const context = shallow(<Header />);
+		expect(context.find('h1').text()).toBe('HODLCON');
+		//expect(context.find(<Link />).length).toBe(3);
+	});
+});
